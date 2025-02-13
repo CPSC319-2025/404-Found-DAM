dcff8f64ef91c97010864b007a80912071a6dc13
Ben Vinnick <benvinnick@gmail.com>
2025-02-12 16:32:51 -0800

    next config fix

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: '/404-Found-DAM',
-  // assetPrefix: '/404-Found-DAM'
+  basePath: '/404-Found-DAM',
+  assetPrefix: '/404-Found-DAM'
 };
 
 export default nextConfig;
