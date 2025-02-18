4ee8b03c19534084ae0d10edafc79983fa16071f
eswlo <sl3923@columbia.edu>
2025-02-18 14:19:32 -0800

    Remove unneeded folders

--- dotnet-backend/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ dotnet-backend/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d59c75ba77f4bdf7d3df58b650e138767b7cd622")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5664408fe8f0e503fdf81ace4b45e3c77053565")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
