b2a57c03c162f875d800b1368fa8b2b46ec9fa2f
Benjamin Vinnick <bvinnick@student.ubc.ca>
2025-02-18 23:34:53 -0800

    prettier/lint

--- frontend/src/app/projects/components/Search.tsx
+++ frontend/src/app/projects/components/Search.tsx
@@ -1,11 +1,11 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Search({}) {
-	return (
-		<input
-			type="text"
-			placeholder="Search..."
-			className="w-1/3 border border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:border-blue-500"
-		/>
-	);
+  return (
+    <input
+      type="text"
+      placeholder="Search..."
+      className="w-1/3 border border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:border-blue-500"
+    />
+  );
 }
