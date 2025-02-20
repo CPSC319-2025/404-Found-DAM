dfb9e756b8e04e48cfd5279fb370ed4dc378a89d
eswlo <sl3923@columbia.edu>
2025-02-19 21:24:38 -0800

    basic setup for grouping and resigtering routes

---
+++ dotnet-backend/APIs/Controllers/ProjectController.cs
@@ -0,0 +1,27 @@
+﻿using Core.Interfaces;
+
+namespace APIs.Controllers
+{
+    public static class ProjectController
+    {
+        public static void MapProjectEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/projects/{projectID}", RetrieveProject).WithName("RetrieveProject").WithOpenApi();
+        }
+
+        private static IResult RetrieveProject(string projectID, ITestService projectService)
+        {
+            var project = projectService.RetrieveProject();
+            return Results.Ok(project);
+            //if (project > 0) 
+            //{
+            //    return Results.Ok(project);
+            //}
+            //else
+            //{
+            //    return Results.NotFound();
+            //}
+        }
+
+    }
+}
---
+++ dotnet-backend/Core/Services/TestService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces;
+
+namespace Core.Services
+{
+    public class TestService : ITestService
+    {
+        public int Val { get; private set; }
+        public TestService()
+        {
+            Val = Random.Shared.Next(1, 1001);
+        }
+        public int RetrieveProject()
+        {
+            return Val;
+        }
+    }
+}
