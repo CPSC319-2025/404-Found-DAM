73f5939c88ddd9128a5a0fcfa2a1ee87330f4d10
eswlo <sl3923@columbia.edu>
2025-03-09 06:00:12 -0700

    Seed mocked users

--- dotnet-backend/APIs/MockedDataSeeding.cs
+++ dotnet-backend/APIs/MockedDataSeeding.cs
@@ -2,7 +2,13 @@ using Core.Services;
 using Core.Interfaces;
 using System.Threading.Tasks;
 using Core.Dtos;
-
+using Core.Entities;
+using Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
 
 namespace MockedData
 {
@@ -10,24 +16,42 @@ namespace MockedData
     {
         public static async Task Seed(IServiceScope scope) 
         {
-            var adminService = scope.ServiceProvider.GetService<IAdminService>();
-            if (adminService != null) 
-            {
-                CreateProjectsReq p = new CreateProjectsReq
-                { 
-                    defaultMetadata = new DefaultMetadata
-                    {
-                        projectName = "seeding test",
-                        location = "Van",
-                        description = "A test",
-                        active = true
-                    },
-                    tags = new List<string> {"t1", "t2"}
-                };
+            var _contextFactory = scope.ServiceProvider.GetService<IDbContextFactory<DAMDbContext>>();
 
-                List<CreateProjectsReq> req = new List<CreateProjectsReq> { p };
-                await adminService.CreateProjects(req);
+            // Add users
+            string filePath = @"..\Core\MockedSeed\mockedUsers.json";
+            string jsonString = File.ReadAllText(filePath);
+            List<User>? users = JsonSerializer.Deserialize<List<User>>(jsonString);
+
+            if (users != null && _contextFactory != null) 
+            {
+                using DAMDbContext _context = _contextFactory.CreateDbContext();
+                await _context.Users.AddRangeAsync(users);
+                await _context.SaveChangesAsync();
             }
+
+
+ 
+
+    
+            // var adminService = scope.ServiceProvider.GetService<IAdminService>();
+            // if (adminService != null) 
+            // {
+            //     CreateProjectsReq p = new CreateProjectsReq
+            //     { 
+            //         defaultMetadata = new DefaultMetadata
+            //         {
+            //             projectName = "seeding test",
+            //             location = "Van",
+            //             description = "A test",
+            //             active = true
+            //         },
+            //         tags = new List<string> {"t1", "t2"}
+            //     };
+
+            //     List<CreateProjectsReq> req = new List<CreateProjectsReq> { p };
+            //     await adminService.CreateProjects(req);
+            // }
         }
     }
 }
