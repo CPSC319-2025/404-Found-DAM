78c439e21766ad1127da55f2db42b12377e48e3a
Parham Aljiani <palijani@students.cs.ubc.ca>
2025-03-11 16:59:04 -0700

    Add FE+BE connection for getting all projects and creating project as admin

--- dotnet-backend/Infrastructure/DataAccess/EFCoreAdminRepository.cs
+++ dotnet-backend/Infrastructure/DataAccess/EFCoreAdminRepository.cs
@@ -142,13 +142,13 @@ namespace Infrastructure.DataAccess
             }
         }
 
-        public async Task<List<Project>> CreateProjectsInDb(List<CreateProjectsReq> req, int userID)
+        public async Task<List<Project>> CreateProjectsInDb(List<CreateProjectsReq> req, int creatorUserID)
         {
             using DAMDbContext _context = _contextFactory.CreateDbContext();
 
-            User? user = await _context.Users.FindAsync(userID);
+            User? creator = await _context.Users.FindAsync(creatorUserID);
 
-            if (user == null) 
+            if (creator == null) 
             {
                 throw new Exception("Failed to create project. User not found.");
             }
@@ -157,9 +157,8 @@ namespace Infrastructure.DataAccess
             List<Tag> tagList = new List<Tag>(); // For storing created Tags
             List<ProjectTag> projectTagList = new List<ProjectTag>(); // For storing created ProjectTags
             List<ProjectMembership> projectMembershipList = new List<ProjectMembership>(); // For storing project memberships
-
+            
             using var transaction = await _context.Database.BeginTransactionAsync(); // To avoid partial data in database in case error occurs
-
             try 
             {
                 foreach (CreateProjectsReq data in req) 
@@ -176,14 +175,61 @@ namespace Infrastructure.DataAccess
                     projectList.Add(newProject);
 
 
-                    ProjectMembership membership = new ProjectMembership
+                    ProjectMembership creatorMembership = new ProjectMembership
                     {
-                        Project = newProject, // EF will handle the ProjectID
-                        UserID = userID,
-                        User = user,
+                        Project = newProject, // EF will assign ProjectID after saving
+                        UserID = creatorUserID,
+                        UserRole = ProjectMembership.UserRoleType.Admin,
+                        User = creator
                     };
-                    projectMembershipList.Add(membership);
+                    projectMembershipList.Add(creatorMembership);
 
+                    // add any specified admins by the project creator to the project
+                    if (data.admins != null && data.admins.Any()) {
+                        foreach (var adminID in data.admins) {
+                            if (adminID == creatorUserID) {
+                                continue;
+                            }
+                            if (projectMembershipList.Any(pm => pm.UserID == adminID && pm.Project == newProject)) {
+                                continue;
+                            }
+                            User? adminUser = await _context.Users.FindAsync(adminID);
+                            if (adminUser == null) {
+                                throw new Exception($"Admin user with ID {adminID} does not exist!");
+                            }
+                            ProjectMembership adminMembership = new ProjectMembership
+                            {
+                                Project = newProject,
+                                UserID = adminID,
+                                UserRole = ProjectMembership.UserRoleType.Admin,
+                                User = adminUser
+                            };
+                            projectMembershipList.Add(adminMembership);
+                        }
+                    }
+                    // add any specified regular users by the project creator to the project
+                    if (data.users != null && data.users.Any()) {
+                        foreach (var userID in data.users) {
+                            if (data.admins != null && data.admins.Contains(userID)) {
+                                continue; //skip, already added
+                            }
+                            if (projectMembershipList.Any(pm => pm.UserID == userID && pm.Project == newProject)) {
+                                continue;
+                            }
+                            User? user = await _context.Users.FindAsync(userID);
+                            if (user == null) {
+                                throw new Exception($"User with ID {userID} not found");
+                            }
+                            ProjectMembership userMembership = new ProjectMembership
+                            {
+                                Project = newProject,
+                                UserID = userID,
+                                UserRole = ProjectMembership.UserRoleType.Regular,
+                                User = user
+                            };
+                            projectMembershipList.Add(userMembership);
+                        }
+                    }
                     if (data.tags != null && data.tags.Any()) 
                     {
                         foreach (string tagName in data.tags) 
