07053b8c235fa11e34d4b4c8191dd65f627cf173
eswlo <sl3923@columbia.edu>
2025-03-14 18:38:24 -0700

    Add assetState and values in UploadAssets

--- dotnet-backend/Infrastructure/DataAccess/PaletteRepository.cs
+++ dotnet-backend/Infrastructure/DataAccess/PaletteRepository.cs
@@ -100,6 +100,7 @@ namespace Infrastructure.DataAccess {
                     MimeType = file.ContentType,
                     ProjectID = null,
                     UserID = request.UserId,
+                    assetState = Asset.AssetStateType.UploadedToPalette
                 };
 
             // Add the asset to the database context and save changes
