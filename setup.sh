ffeb67e9626d221c94272c98f78a8ddb180fa377
aradsab <ediarad@yahoo.co.uk>
2025-03-19 11:15:33 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -63,9 +63,11 @@ fi
 #############################################
 
 echo "Starting MySQL container"
-docker run --platform linux/amd64 -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=LetsGoTeam!' -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
-
+# Check if container is already running
+CONTAINER_NAME="mssql-server"
+docker run --platform linux/amd64 -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=LetsGoTeam!' -p 1433:1433 -d --name $CONTAINER_NAME mcr.microsoft.com/mssql/server:2019-latest
 echo "Waiting for MySQL to initialize (this may take a few seconds)..."
+sleep 30
 
 #############################################
 # Verify Node and npm Versions for React Frontend
