805366fef1871a3a46f39567a0631db47f8312a6
aradsab <ediarad@yahoo.co.uk>
2025-03-20 09:19:55 -0700

    we need to be using string for Blob Id

--- dotnet-backend/Core/Interfaces/IPaletteRepository.cs
+++ dotnet-backend/Core/Interfaces/IPaletteRepository.cs
@@ -4,15 +4,13 @@ using Microsoft.AspNetCore.Http;
 
 namespace Core.Interfaces  {
     public interface IPaletteRepository {
-        public Task<List<Asset>> GetAssetsFromPalette();
-
         public Task<List<string>> GetProjectTagsAsync(int projectId);
 
         public Task<bool> AddTagsToPaletteImagesAsync(List<int> imageIds, List<string> tags);
 
         public Task<bool> DeleteAsset(DeletePaletteAssetReq request);
 
-        public Task<int> UploadAssets(IFormFile file, UploadAssetsReq request);
+        public Task<string> UploadAssets(IFormFile file, UploadAssetsReq request);
         public Task<List<IFormFile>> GetAssetsAsync(int userId);
     }
 }
