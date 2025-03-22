e51061abf2bacbb35798525ca0b28b78f52f369a
eswlo <sl3923@columbia.edu>
2025-03-22 00:29:41 -0700

    Edit UploadAssetsReq to be more descriptive

--- dotnet-backend/Core/Dtos/PaletteService/UploadAssetsReq.cs
+++ dotnet-backend/Core/Dtos/PaletteService/UploadAssetsReq.cs
@@ -2,8 +2,8 @@ namespace Core.Dtos
 {
     public class UploadAssetsReq
     {
-        public required string Name { get; set; }
-        public required string Type { get; set; }
+        public required string UploadTaskName { get; set; }
+        public required string AssetMimeType { get; set; }
         public required int UserId { get; set; }
 
         public int ProjectID { get; set; }
