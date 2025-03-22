78e489b9a92cc93032de06a055d0688ffc36d7bc
eswlo <sl3923@columbia.edu>
2025-03-22 00:30:26 -0700

    Add convertToWebp to check if FE requires webp conversion

--- dotnet-backend/Core/Interfaces/IPaletteService.cs
+++ dotnet-backend/Core/Interfaces/IPaletteService.cs
@@ -9,8 +9,8 @@ namespace Core.Interfaces
 {
     public interface IPaletteService
     {
-        Task<int> ProcessUploadAsync(IFormFile file, UploadAssetsReq request);
-        Task<object[]> ProcessUploadsAsync(List<IFormFile> files, UploadAssetsReq request);
+        Task<int> ProcessUploadAsync(IFormFile file, UploadAssetsReq request, bool convertToWebp);
+        Task<object[]> ProcessUploadsAsync(List<IFormFile> files, UploadAssetsReq request, bool convertToWebp);
 
         Task<bool> DeleteAssetAsync(DeletePaletteAssetReq request);
         Task<List<IFormFile>> GetAssets(GetPaletteAssetsReq request);
