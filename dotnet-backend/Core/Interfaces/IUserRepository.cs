acf9b03d411c99befc293744e931646c3286b0f7
yassersan <yasser.sanobar@gmail.com>
2025-03-22 23:52:11 -0700

    removed allowedorigins from appsettings

--- dotnet-backend/Core/Interfaces/IUserRepository.cs
+++ dotnet-backend/Core/Interfaces/IUserRepository.cs
@@ -1,13 +1,4 @@
 using Core.Entities;
-
-namespace Core.Interfaces
-{
-    public interface IUserRepository
-    {
-        Task<User?> GetUserByEmailAsync(string email);
-    }
-}
-using Core.Entities;
 using Core.Dtos;
 
 namespace Core.Interfaces
