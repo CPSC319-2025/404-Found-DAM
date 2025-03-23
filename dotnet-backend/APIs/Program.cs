ed5ac21329b078cfd48ad47063c25b2bc45ead03
aradsab <ediarad@yahoo.co.uk>
2025-03-23 10:16:51 -0700

    revert seed

--- dotnet-backend/APIs/Program.cs
+++ dotnet-backend/APIs/Program.cs
@@ -111,7 +111,6 @@ if (app.Environment.IsDevelopment())
     
     // Apply migrations to create a new database
     dbContext.Database.Migrate();
-    await SeedDatabase(app);
     
     Console.WriteLine("Database was reset and migrations applied successfully");
 } else
