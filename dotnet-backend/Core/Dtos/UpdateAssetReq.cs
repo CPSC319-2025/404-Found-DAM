cc666a24cda44baf31072838a850b119fd3ff1e4
aradsab <ediarad@yahoo.co.uk>
2025-04-01 16:24:02 -0700

    removed OriginalFileName

--- dotnet-backend/Core/Dtos/UpdateAssetReq.cs
+++ dotnet-backend/Core/Dtos/UpdateAssetReq.cs
@@ -5,7 +5,6 @@ namespace Core.Dtos
     public class UpdateAssetReq
     {
         public string BlobId { get; set; }
-        public string OriginalFileName { get; set; }
         public string AssetMimeType { get; set; }
         public int UserId { get; set; }
     }
