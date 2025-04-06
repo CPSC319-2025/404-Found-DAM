270b5836116f65250f1632ebe1585eeb39f7fb4e
theseanx <111398191+theseanx@users.noreply.github.com>
2025-04-06 14:33:08 -0700

    temp changes - add log when admin configures logs for all projects

--- dotnet-backend/APIs/Controllers/TagController.cs
+++ dotnet-backend/APIs/Controllers/TagController.cs
@@ -7,12 +7,24 @@ namespace APIs.Controllers
 {
     public static class TagController
     {
+
+        private const bool AdminActionTrue = true;
+
+        private const bool logDebug = false;
+
+        private const bool verboseLogs = false;
         public static void MapTagEndpoints(this WebApplication app)
         {
             app.MapGet("/tags", GetTags)
                .WithName("GetTags")
                .WithOpenApi();
             app.MapPut("/tags", ReplaceTags).WithName("ReplaceTags").WithOpenApi();
+        }
+
+        private static IServiceProvider GetServiceProvider(HttpContext context)
+        {
+            return context.RequestServices; // for activity log
+
         }
 
         private static async Task<IResult> GetTags(ITagService tagService)
@@ -40,10 +52,32 @@ namespace APIs.Controllers
         {
             try
             {
+                // Get services from IServiceProvider
+                var serviceProvider = GetServiceProvider(context);
+                var activityLogService = serviceProvider.GetRequiredService<IActivityLogService>();
+                var projectService = serviceProvider.GetRequiredService<IProjectService>();
+                var userService = serviceProvider.GetRequiredService<IUserService>();
+                // TODO: Check requester is a super admin in DB
+                int requesterID = Convert.ToInt32(context.Items["userId"]);
+
+
                 var tagDtos = await context.Request.ReadFromJsonAsync<IEnumerable<CreateTagDto>>();
                 if (tagDtos == null) return Results.BadRequest("Invalid tag data");
 
                 await tagService.ReplaceAllTagsAsync(tagDtos);
+
+                // await activityLogService.AddLogAsync(new CreateActivityLogDto
+                // {
+                //     // Add log (done)
+                //     userID = requesterID,
+                //     changeType = "Update Logs",
+                //     description = theDescription,
+                //     projID = "",
+                //     assetID = "",
+                //     isAdminAction = AdminActionTrue
+                // });
+
+
                 return Results.Ok("Tag table replaced");
             }
             catch (Exception ex)
